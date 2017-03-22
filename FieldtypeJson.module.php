e5fd1b6ef60422ce59600b27c45c78e3661ec9cc
Adam Kiss <iam@adamkiss.com>
2017-03-22 14:55:47 +0100

    Add WireJson to Fieldtype

--- FieldtypeJson.module.php
+++ FieldtypeJson.module.php
@@ -3,4 +3,11 @@
 /**
 * Fieldtype that stores arbitrary data as JSON text (for ease of editability in PW)
 */
-class FieldtypeJson extends FieldtypeTextarea {}
+class FieldtypeJson extends FieldtypeTextarea {
+
+  public function init() {
+    parent::init();
+    require_once dirname(__FILE__) . '/WireJson.php';
+  }
+
+}
