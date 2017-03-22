6bc82ba50bee9356ff175de91c4c2dde2b8ccda6
Adam Kiss <iam@adamkiss.com>
2017-03-22 16:19:04 +0100

    WireJson object ready for 0.5

--- WireJson.php
+++ WireJson.php
@@ -3,4 +3,15 @@
 /**
 * Object to encode JSON into from FieldtypeJson
 */
-class WireJson extends WireData {}
+class WireJson extends WireData {
+
+  public function fromJson($jsonString) {
+    $jsonDecoded = json_decode($jsonString);
+    return $this->setArray(get_object_vars($jsonDecoded));
+  }
+
+  public function toJson() {
+    return wireEncodeJSON($this->getArray(), true);
+  }
+
+}
--- spec/WireJson.spec.php
+++ spec/WireJson.spec.php
@@ -2,21 +2,54 @@
   require_once dirname(__FILE__) . '/../vendor/autoload.php';
   require_once dirname(__FILE__) . '/../WireJson.php';
 
-  describe("WireJson", function(){
+  const JSON_SIMPLE = '{"name":"Barry","surname":"Goodman","years":[1960,1964,1968]}';
+  const JSON_OBJECT = '{"name":"Song of Winners","author":'.JSON_SIMPLE.'}';
 
+  describe("WireJson", function(){
 
     describe("-> fromJson", function(){
       it("imports simple JSON", function(){
 
         $test = new \ProcessWire\WireJson();
-        $json1 = '{"name":"Barry", "surname":"Goodman", "years": [1960, 1964, 1968]}';
+        $test->fromJson(JSON_SIMPLE);
 
-        $test->fromJson($json1);
-
+        expect(get_class($test))->toBe('ProcessWire\\WireJson');
         expect($test->name)->toBe('Barry');
         expect($test->surname)->toBe('Goodman');
         expect($test->years)->toBe([1960, 1964, 1968]);
 
+      });
+
+      it("imports objects WireJSON", function(){
+
+        $test = new \ProcessWire\WireJson();
+        $test->fromJson(JSON_OBJECT);
+
+        expect($test->name)->toBe('Song of Winners');
+        expect(gettype($test->author))->toBe('object');
+        $test->author = (object)[
+          'years' => [1960, 1964, 1968]
+        ];
+        expect($test->author->years)->toBe([1960, 1964, 1968]);
+      });
+    });
+
+    describe("-> toJson", function(){
+      it("exports simple JSON", function(){
+
+        $test = new \ProcessWire\WireJson();
+        $test->fromJson(JSON_SIMPLE);
+
+        expect($test->toJson())->toBe(JSON_SIMPLE);
+      });
+
+      it("exports self as object", function(){
+
+        $test = new \ProcessWire\WireJson();
+        $test->fromJson(JSON_OBJECT);
+        $test->author = json_decode(JSON_SIMPLE);
+
+        expect($test->toJson())->toBe(JSON_OBJECT);
       });
     });
 
